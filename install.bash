b2aed2b69750ca9f5eb3f95c9c43f027a3941848
Tom W. Yedwab <tom@khanacademy.org>
2012-04-18 21:55:50 -0700

    Use lighttpd to redirect requests to search-rpc.khanacademy.org.

--- install.bash
+++ install.bash
@@ -14,3 +14,10 @@ sudo chown -R tomcat6:tomcat6 solr
 
 # Restart Tomcat server to pick up our changes
 sudo /etc/init.d/tomcat6 restart
+
+# Configure lighttpd to redirect requests:
+#   search-rpc.khanacademy.org => localhost:9001 (Tomcat)
+sudo mv lighttpd.conf /etc/lighttpd/lighttpd.conf
+
+# Restart lighttpd to pick up our changes
+sudo /etc/init.d/lighttpd restart
