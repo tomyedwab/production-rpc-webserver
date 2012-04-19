0d2718c5dd77401606dcd95805e214c86b838bde
Tom W. Yedwab <tom@khanacademy.org>
2012-04-18 22:02:51 -0700

    Oops. cp not mv.

--- install.bash
+++ install.bash
@@ -17,7 +17,7 @@ sudo /etc/init.d/tomcat6 restart
 
 # Configure lighttpd to redirect requests:
 #   search-rpc.khanacademy.org => localhost:9001 (Tomcat)
-sudo mv lighttpd.conf /etc/lighttpd/lighttpd.conf
+sudo cp lighttpd.conf /etc/lighttpd/lighttpd.conf
 
 # Restart lighttpd to pick up our changes
 sudo /etc/init.d/lighttpd restart
