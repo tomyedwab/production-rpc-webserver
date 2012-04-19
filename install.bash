64459faca4b39212f3d0aa9581a60ea2b9346e53
Tom W. Yedwab <tom@khanacademy.org>
2012-04-18 21:18:39 -0700

    Set permissions properly.

--- install.bash
+++ install.bash
@@ -9,5 +9,8 @@ sudo sed -i 's/8080/9001/g' /var/lib/tomcat6/conf/server.xml
 # Copy configuration data
 sudo cp solr_tomcat_context.xml /var/lib/tomcat6/conf/Catalina/localhost/solr.xml
 
+# Set permissions so that Tomcat can read the files
+sudo chown -R tomcat6:tomcat6 solr
+
 # Restart Tomcat server to pick up our changes
 sudo /etc/init.d/tomcat6 restart
