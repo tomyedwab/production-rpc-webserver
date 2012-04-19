5e6f93f7b15b0ac652af20a531cadcd15f278a98
Tom W. Yedwab <tom@khanacademy.org>
2012-04-18 20:45:29 -0700

    Basic Solr installation script.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ production-rpc-webserver
 ========================
 
 Production RPC servers for Khan Academy website features hosted in EC2.
+
--- install.bash
+++ install.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 # Start out by installing package dependencies
+sudo apt-get install lighttpd tomcat6
 
-sudo apt-get install lighttpd
+# Configure Tomcat: Set port number to 9001
+sudo sed -i 's/8080/9001/g' /var/lib/tomcat6/conf/server.xml
+
+# Copy configuration data
+sudo cp solr_tomcat_context.xml /var/lib/tomcat6/conf/Catalina/localhost/solr.xml
+
+# Restart Tomcat server to pick up our changes
+sudo /etc/init.d/tomcat6 restart
